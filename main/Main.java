5a102faf4796fca72a867c959d567271111e1167
Elliot Rourke <ywb16155@uni.strath.ac.uk>
2018-02-21 16:10:14 +0000

    Cleaning

--- main/Main.java
+++ main/Main.java
@@ -14,41 +14,7 @@ import java.awt.event.KeyEvent;
 public class Main {
 
 	public static void main(String[] args) {
-		try {
-			// Use the platform look and feel
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (Exception e) {
-			System.out.println("Look and Feel error in Main");
-		}
-
-		Model model = new Model();
-
-		model.setBallSpeed(200, 200);
-
-		// Vertical line at (100,100), width 300
-		//model.addLine(new VerticalLine(90, 100, 300));
-		/*//model.addLine(new VerticalLine(100, 122, 300));
-		model.addLine(new VerticalLine(100, 200, 300));
-		model.addLine(new VerticalLine(100, 300, 300));
-		model.addLine(new VerticalLine(100, 400, 300));*/
-
-		/*model.addSquare(new Square(50,50,20));
-		model.addTriangle(new Triangle(100,100,20));
-		model.addSquare(new Square(150,150,20));
-		model.addTriangle(new Triangle(200,200,20));
-		model.addSquare(new Square(250,250,20));
-		model.addTriangle(new Triangle(300,300,25));
-		model.addSquare(new Square(350,350,20));
-		model.addSquare(new Square(400,400,20));
-		model.addSquare(new Square(450,450,20));
-*/
-
-		//FLIPPER TEST
-		model.addCircle(new Circle(70,340,40));
-		model.addSquare(new Square(70,340,40));
-		model.addFlipper(new Flipper(70,340,70,380,"LEFT"));
-		model.addFlipper(new Flipper(150,340,150,380,"RIGHT"));
-		RunGui gui = new RunGui(model);
-		gui.createAndShowGUI();
+		
 	}
+
 }
