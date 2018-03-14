16f23504465e7e313d091a966a67a67475f482a5
Mohammed Farhan Jalil <mohammed.jalil.2015@uni.strath.ac.uk>
2018-03-14 14:37:38 +0000

    all set for testing

---
+++ tests/ModelTests.java
@@ -0,0 +1,26 @@
+package tests;
+
+import model.Model;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+public class ModelTests {
+    private Model ml;
+
+
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void testRemoveBall(){
+
+
+    }
+
+}
