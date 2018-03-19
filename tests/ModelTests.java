d0b99495242a01e849be223e9b650443da7dbba7
pavindersingh <dkb15175@uni.strath.ac.uk>
2018-03-19 03:51:46 +0000

    JUnit: Adding More JUnit Tests for coverage

--- tests/ModelTests.java
+++ tests/ModelTests.java
@@ -48,6 +48,21 @@ public class ModelTests {
         assertFalse(model.addGizmo("triangle", "T0", "1", "1"));
     }
 
+    @Test
+    public void testRotateGizmo(){
+        model.addGizmo("triangle", "T0", "2", "2");
+        model.addGizmo("absorber", "A0", "3", "3");
+        model.addGizmo("square", "S0", "4", "4");
+
+        model.addGizmo("leftflipper", "LF0", "3", "3");
+        model.addGizmo("rightflipper", "RF0", "8", "3");
+
+
+        //roate Gizmos
+
+
+    }
+
 
 
 }
